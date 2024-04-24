696c71ab95c55f9c165a4761ec6c2e012784451f
Sri-Varshini-Sukumar-12 <167115224+Sri-Varshini-Sukumar-12@users.noreply.github.com>
2024-04-24 23:19:27 +0530

    Update First_Half_Second_Half_String.c

--- First_Half_Second_Half_String.c
+++ First_Half_Second_Half_String.c
@@ -1,5 +1,6 @@
 /*The program must accept a string S in lowercase characters. The program must print the 
-characters from (a-m) in the first half and (n-z) in the second part*/
+characters from (a-m) in the first half and (n-z) in the second half
+*/
 
 /*Example Input/Ouput 1:
 Input : electronics
